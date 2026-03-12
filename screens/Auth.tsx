7dbf003315f239ce2fb66f675a13eb02cf0d8e7f
sarshanoddin-max <sarshanoddin@gmail.com>
2026-03-12 16:03:59 +0530

    Create Auth.tsx

---
+++ screens/Auth.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Auth() {
+  return (
+    <div style={{padding:20}}>
+      <h2>Login Page</h2>
+      <p>App working successfully</p>
+    </div>
+  );
+}
